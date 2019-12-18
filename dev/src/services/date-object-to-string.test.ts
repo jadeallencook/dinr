3376865aca8150ec8f926fbf02cd42c8f731233d
Jade Allen Cook <jadeallencook@gmail.com>
2019-12-17 23:43:35 -0800

    🚀 Updated date object to string service and tests

--- dev/src/services/date-object-to-string.test.ts
+++ dev/src/services/date-object-to-string.test.ts
@@ -1,26 +1,29 @@
 import dateObjectToString from './date-object-to-string';
+import { Date } from '../interfaces';
 
-test('Description of test', () => {
-  //odd input
-  expect(dateObjectToString({day: null, month: null, year: null})).toBe(false);
-  expect(dateObjectToString({day: 1, month: true, year: 20})).toBe(false);
+test('1/1/2020 returns 1012020', () => {
+  const date: Date = { 
+    month: '1',
+    day: '1',  
+    year: '2020' 
+  };
+  expect(dateObjectToString(date)).toBe('1012020');
+});
 
-  //big nums
-  expect(dateObjectToString({day: "1", month: "100", year: "2020"})).toBe(false);
-  expect(dateObjectToString({day: 1, month: 100, year: 2020})).toBe(false);
-  expect(dateObjectToString({day: "100", month: "1", year: "2020"})).toBe(false);
-  expect(dateObjectToString({day: 100, month: 1, year: 2020})).toBe(false);
+test('12/12/2020 returns 12122020', () => {
+  const date: Date = { 
+    month: '12',
+    day: '12',  
+    year: '2020' 
+  };
+  expect(dateObjectToString(date)).toBe('12122020');
+});
 
-  //negatives
-  expect(dateObjectToString({day: -1, month: 1, year: 2020})).toBe(false);
-  expect(dateObjectToString({day: 1, month: -1, year: 2020})).toBe(false);
-  expect(dateObjectToString({day: 1, month: 1, year: -2020})).toBe(false)
-  expect(dateObjectToString({day: "-1", month: "1", year: "2020"})).toBe(false);
-  expect(dateObjectToString({day: "1", month: "-1", year: "2020"})).toBe(false);
-  expect(dateObjectToString({day: "1", month: "1", year: "-2020"})).toBe(false);
-  
-  //correct runs
-  expect(dateObjectToString({day: "1", month: "1", year: "2020"})).toBe('1012020');
-  expect(dateObjectToString({day: 1, month: 1, year: 2020})).toBe('1012020');
-  expect(dateObjectToString({day: 1, month: "1", year: "2020"})).toBe('1012020');
+test('12/1/2020 returns 12012020', () => {
+  const date: Date = { 
+    month: '12',
+    day: '1',  
+    year: '2020' 
+  };
+  expect(dateObjectToString(date)).toBe('12012020');
 });
--- dev/src/services/date-object-to-string.ts
+++ dev/src/services/date-object-to-string.ts
@@ -1,50 +1,6 @@
-/*
-    Converts date object to string
-    {
-        day: "1",
-        month: "1",
-        year: "2020"
-    } => "1012020"
-*/
+import { Date } from '../interfaces';
 
-export default (date: object): string | false => {
-    let string = '';
-
-    //month
-    if(date.month == null || (typeof date.month == typeof true)) {
-        return false;
-    } else {
-        if(0 <= Number(date.month) && Number(date.month) <= 12) {
-            string += date.month;
-        } else {
-            return false;
-        }
-    }
-
-    //day
-    if(date.day == null || (typeof date.day == typeof true)) {
-        return false;
-    } else {
-        if(0 <= Number(date.day) && Number(date.day) <= 12) {
-            if(Number(date.day) < 10) {
-                string += 0;
-            }
-            string += date.month;
-        } else {
-            return false;
-        }
-    }
-
-    //year
-    if(date.year == null || (typeof date.year == typeof true)) {
-        return false;
-    } else {
-        if(0 < Number(date.year)) {
-            string += date.year;
-        } else {
-            return false;
-        }
-    }
- 
-    return string;
+export default (date: Date): string => {
+    let { month, day, year } = date;
+    return `${month}${day.length === 1 ? `0${day}`: day}${year}`;
 }
