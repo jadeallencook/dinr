4e5e32d1a79665516ba4a1b841c0683aea45b259
Clayton Cook <claytoncook@o-5.local>
2019-12-19 18:10:48 -0500

    ⏰Updated Military To Standard Time

--- dev/src/services/military-to-standard-time.test.ts
+++ dev/src/services/military-to-standard-time.test.ts
@@ -11,6 +11,21 @@ test('1 returns 1:00AM', () => {
   expect(militaryToStandardTime(time)).toBe('1:00AM');
 });
 
+test('6 returns 6:00AM', () => {
+  const time: Time = 6;
+  expect(militaryToStandardTime(time)).toBe('6:00AM');
+});
+
+test('6 returns 11:00AM', () => {
+  const time: Time = 11;
+  expect(militaryToStandardTime(time)).toBe('11:00AM');
+});
+
+test('12 returns 12:00PM', () => {
+  const time: Time = 12;
+  expect(militaryToStandardTime(time)).toBe('12:00PM');
+});
+
 test('13 returns 1:00PM', () => {
   const time: Time = 13;
   expect(militaryToStandardTime(time)).toBe('1:00PM');
--- dev/src/services/military-to-standard-time.ts
+++ dev/src/services/military-to-standard-time.ts
@@ -1,6 +1,5 @@
 import {Time} from '../interfaces';
 
 export default (military: Time): string => {
-    let string = '';
-    return string;
+    return military == 0 ? `12:00AM` : military == 12 ? '12:00PM' : military > 12 ? `${military-12}:00PM` : `${military}:00AM`;
 }
