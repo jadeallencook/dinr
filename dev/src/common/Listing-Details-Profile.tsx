4ebeab4a942faae2d92c93ae9d8a7c3ac353ec66
Clayton Cook <claytoncook@o-5.local>
2019-12-20 19:43:03 -0500

    👤Decoupled Listing Detail Profile SCSS

--- dev/src/common/Listing-Details-Profile.tsx
+++ dev/src/common/Listing-Details-Profile.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Profiles from '../assets/profiles-snapshot.json';
 import acceptedPayments from '../services/accepted-payments';
+import './Listing-Details-Profile.scss';
 
 interface ListingDetailsProfileProps {
     profile: any;
@@ -10,7 +11,7 @@ const ListingDetailsProfile: React.FC<ListingDetailsProfileProps> = props => {
     const profile = Profiles[props.profile];
     const { name, street } = profile.personal;
     return (
-        <div>
+        <div className="Listing-Details-Profile">
             <h3>Hosted by {name}</h3>
             <ul>
                 <li><b>Address: </b>{street}</li>
