a8bb81593861385c43afe206739b4475e9290b9e
Jade Allen Cook <jadeallencook@gmail.com>
2019-12-29 20:35:08 -0800

    🗄 Moved dev file to root and exported build

---
+++ build/precache-manifest.f575dc476464d713e4d7398c930ef4c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee18ec2ee06af0ec7392c2a10e6949e",
+    "url": "/Dinr/build/index.html"
+  },
+  {
+    "revision": "a1b8e33d2e8b9a4f11bb",
+    "url": "/Dinr/build/static/css/main.9ce7511f.chunk.css"
+  },
+  {
+    "revision": "518f437f17b835e8f273",
+    "url": "/Dinr/build/static/js/2.18eaaf33.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Dinr/build/static/js/2.18eaaf33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1b8e33d2e8b9a4f11bb",
+    "url": "/Dinr/build/static/js/main.7a3f39da.chunk.js"
+  },
+  {
+    "revision": "d8c1751648ad99494eee",
+    "url": "/Dinr/build/static/js/runtime-main.5592a702.js"
+  },
+  {
+    "revision": "5aa57b039a2022da41c022a3fb620cf9",
+    "url": "/Dinr/build/static/media/banner.5aa57b03.jpeg"
+  }
+]);
