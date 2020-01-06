33a8e411cdd78ec68a0de6309cadec37483f6144
Clayton Cook <claytoncook@o-5.local>
2020-01-06 14:23:31 -0500

    💸Updated Accepted Payments

--- src/services/accepted-payments.ts
+++ src/services/accepted-payments.ts
@@ -1,3 +1,21 @@
 export default (payments: any): string => {
-    return `Cash and Venmo`;
+    let paymentOptions = [];
+
+    paymentOptions.push(payments.cash ? `Cash` : null);
+    paymentOptions.push(payments.venmo ? `Venmo` : null);
+    paymentOptions.push(payments.paypal ? `Paypal` : null);
+    paymentOptions.push(payments.cashapp ? `Cash App` : null);
+
+    let filteredPaymentOptions = paymentOptions.filter(Boolean);
+
+    if(filteredPaymentOptions.length === 1)
+        return `Only ${filteredPaymentOptions[0]}`
+    else if(filteredPaymentOptions.length === 2)
+        return `${filteredPaymentOptions[0]} and ${filteredPaymentOptions[1]}`;
+    else if(filteredPaymentOptions.length === 3)
+        return `${filteredPaymentOptions[0]}, ${filteredPaymentOptions[1]}, and ${filteredPaymentOptions[2]}`;
+    else if(filteredPaymentOptions.length === 4)
+        return `${filteredPaymentOptions[0]}, ${filteredPaymentOptions[1]}, ${filteredPaymentOptions[2]}, and ${filteredPaymentOptions[3]}`;
+    else
+        return `No Payment`;
 }
