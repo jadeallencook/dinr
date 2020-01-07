f1962d89a3534c4784b18d12933b2bc53726e9eb
Jade Allen Cook <jadeallencook@gmail.com>
2020-01-06 20:25:49 -0800

    ⚙️ Added payment service to listing

--- src/common/Listing-Details-Profile.tsx
+++ src/common/Listing-Details-Profile.tsx
@@ -9,13 +9,15 @@ interface ListingDetailsProfileProps {
 
 const ListingDetailsProfile: React.FC<ListingDetailsProfileProps> = props => {
     const profile = Profiles[props.profile];
-    const { name, street } = profile.personal;
+    const { payment, personal } = profile;
+    const { name, street } = personal;
+    console.log(payment);
     return (
         <div className="Listing-Details-Profile">
             <h3>Hosted by {name}</h3>
             <ul>
                 <li><b>Address: </b>{street}</li>
-                <li><b>Payments: </b>{acceptedPayments('')}</li>
+                <li><b>Payments: </b>{acceptedPayments(payment)}</li>
             </ul>
         </div>
     );
