991570a052788b720650c1b62625810a31ebb1dc
Jade Allen Cook <jadeallencook@gmail.com>
2020-01-06 21:13:43 -0800

    ✏️ Added descriptions to test file

--- src/services/accepted-payments.test.ts
+++ src/services/accepted-payments.test.ts
@@ -1,7 +1,7 @@
 import acceptedPayments from './accepted-payments';
 import { Payment } from '../interfaces';
 
-test('', ()=> {
+test('No Payment', ()=> {
     const payments: Payment = {
             cash: false,
             venmo: false,
@@ -11,7 +11,7 @@ test('', ()=> {
     expect(acceptedPayments(payments)).toBe('No Payment');
 });
 
-test('', ()=> {
+test('Only Cash', ()=> {
     const payments: Payment = {
             cash: true,
             venmo: false,
@@ -21,7 +21,17 @@ test('', ()=> {
     expect(acceptedPayments(payments)).toBe('Only Cash');
 });
 
-test('', ()=> {
+test('Only Venmo', ()=> {
+    const payments: Payment = {
+            cash: false,
+            venmo: true,
+            paypal: false,
+            cashapp: false
+    }
+    expect(acceptedPayments(payments)).toBe('Only Venmo');
+});
+
+test('Cash and Venmo', ()=> {
     const payments: Payment = {
             cash: true,
             venmo: true,
@@ -31,32 +41,32 @@ test('', ()=> {
     expect(acceptedPayments(payments)).toBe('Cash and Venmo');
 });
 
-test('', ()=> {
+test('Cash, Venmo, and PayPal', ()=> {
     const payments: Payment = {
             cash: true,
             venmo: true,
             paypal: true,
             cashapp: false
     }
-    expect(acceptedPayments(payments)).toBe('Cash, Venmo, and Paypal');
+    expect(acceptedPayments(payments)).toBe('Cash, Venmo, and PayPal');
 });
 
-test('', ()=> {
+test('Cash, Venmo, PayPal, and Cash App', ()=> {
     const payments: Payment = {
             cash: true,
             venmo: true,
             paypal: true,
             cashapp: true
     }
-    expect(acceptedPayments(payments)).toBe('Cash, Venmo, Paypal, and Cash App');
+    expect(acceptedPayments(payments)).toBe('Cash, Venmo, PayPal, and Cash App');
 });
 
-test('', ()=> {
+test('PayPal and Cash App', ()=> {
     const payments: Payment = {
             cash: false,
             venmo: false,
             paypal: true,
             cashapp: true
     }
-    expect(acceptedPayments(payments)).toBe('Paypal and Cash App');
+    expect(acceptedPayments(payments)).toBe('PayPal and Cash App');
 });
