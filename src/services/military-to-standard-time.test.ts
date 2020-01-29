61a4a758b8dfa263d9ee71df3b80e5547298b4b5
24993473 <claytonleonardcook@gmail.com>
2020-01-28 19:49:47 -0500

    ⏰Updated Military to Standard Service

--- src/services/military-to-standard-time.test.ts
+++ src/services/military-to-standard-time.test.ts
@@ -1,32 +1,54 @@
 import militaryToStandardTime from './military-to-standard-time';
-import { Time } from '../interfaces';
 
 test('0 returns 12:00AM', () => {
-  const time: Time = 0;
+  const time = 0;
   expect(militaryToStandardTime(time)).toBe('12:00AM');
 });
-
-test('1 returns 1:00AM', () => {
-  const time: Time = 1;
+test('61 returns 1:01AM', () => {
+  const time = 61;
+  expect(militaryToStandardTime(time)).toBe('1:01AM');
+});
+test('90 returns 1:30AM', () => {
+  const time = 90;
+  expect(militaryToStandardTime(time)).toBe('1:30AM');
+});
+test('101 returns 1:01AM', () => {
+  const time = 101;
+  expect(militaryToStandardTime(time)).toBe('1:01AM');
+});
+test('"0:0" returns 12:00AM', () => {
+  const time = "0:0";
+  expect(militaryToStandardTime(time)).toBe('12:00AM');
+});
+test('13 returns 12:13AM', () => {
+  const time = 13;
+  expect(militaryToStandardTime(time)).toBe('12:13AM');
+});
+test('1300 returns 1:00PM', () => {
+  const time = 1300;
+  expect(militaryToStandardTime(time)).toBe('1:00PM');
+});
+test('"13:00" returns 1:00PM', () => {
+  const time = "13:00";
+  expect(militaryToStandardTime(time)).toBe('1:00PM');
+});
+test('"1300" returns 1:00PM', () => {
+  const time = "1300";
+  expect(militaryToStandardTime(time)).toBe('1:00PM');
+});
+test('"48:00" returns 12:00PM', () => {
+  const time = "48:00";
+  expect(militaryToStandardTime(time)).toBe('12:00AM');
+});
+test('"49:00" returns 1:00AM', () => {
+  const time = "49:00";
   expect(militaryToStandardTime(time)).toBe('1:00AM');
 });
-
-test('6 returns 6:00AM', () => {
-  const time: Time = 6;
-  expect(militaryToStandardTime(time)).toBe('6:00AM');
+test('134 returns 1:34AM', () => {
+  const time = 134;
+  expect(militaryToStandardTime(time)).toBe('1:34AM');
 });
-
-test('6 returns 11:00AM', () => {
-  const time: Time = 11;
-  expect(militaryToStandardTime(time)).toBe('11:00AM');
-});
-
-test('12 returns 12:00PM', () => {
-  const time: Time = 12;
-  expect(militaryToStandardTime(time)).toBe('12:00PM');
-});
-
-test('13 returns 1:00PM', () => {
-  const time: Time = 13;
-  expect(militaryToStandardTime(time)).toBe('1:00PM');
+test('1361 returns 2:01PM', () => {
+  const time = 1361;
+  expect(militaryToStandardTime(time)).toBe('2:01PM');
 });
--- src/services/military-to-standard-time.ts
+++ src/services/military-to-standard-time.ts
@@ -1,13 +1,13 @@
-import {Time} from '../interfaces';
-
-export default (military: Time): string => {
-    if (military === 0) {
-        return `12:00AM`;
-    } else if (military < 1200) {
-        return `${military}:00AM`;
-    } else if (military === 1200) {
-        return `12:00PM`;
-    } else {
-        return `${military-12}:00PM`;
+export default (military: string | number): string => {
+    if(typeof(military) === 'string') {
+        military = Number(military.includes(':') ? military.replace(':', '') : military)
     }
+    let minutes = (military%100), hours = (military - minutes)/100
+    if(minutes >= 60) {
+        hours++
+        minutes -= 60
+    }
+    let result = `${(hours%12 == 0) ? '12' : hours%12}:${String(minutes).padStart(2, '0')}`
+    result += Math.floor(hours/12) % 2 == 0 ? 'AM' : 'PM'
+    return result
 }
