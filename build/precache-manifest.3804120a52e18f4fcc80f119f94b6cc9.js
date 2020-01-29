925e6626389325121fb8a3c552f497eff3fb526f
Jade Allen Cook <jadeallencook@gmail.com>
2020-01-28 21:10:51 -0800

    🚀 Exported production build

---
+++ build/precache-manifest.3804120a52e18f4fcc80f119f94b6cc9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bff30b066651eaa4aa88861f9439db7",
+    "url": "/Dinr/build/index.html"
+  },
+  {
+    "revision": "228d8dac5c248acdeba0",
+    "url": "/Dinr/build/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "e9673ff06e3a508f35a5",
+    "url": "/Dinr/build/static/css/main.03bceed3.chunk.css"
+  },
+  {
+    "revision": "228d8dac5c248acdeba0",
+    "url": "/Dinr/build/static/js/2.1907301e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Dinr/build/static/js/2.1907301e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9673ff06e3a508f35a5",
+    "url": "/Dinr/build/static/js/main.29ef4492.chunk.js"
+  },
+  {
+    "revision": "2b665b954fc17cb4551d",
+    "url": "/Dinr/build/static/js/runtime-main.31a4375a.js"
+  },
+  {
+    "revision": "29733819982066ade9408198b8b6534f",
+    "url": "/Dinr/build/static/media/dinr-logo.29733819.svg"
+  }
+]);
