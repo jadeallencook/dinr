409d652a8d7f5c5833057de85946c401fb70e475
24993473 <claytonleonardcook@gmail.com>
2020-01-30 00:25:02 -0500

    ⏰Updated Military to Standard Helper

--- src/helpers/military-to-standard-time.tsx
+++ src/helpers/military-to-standard-time.tsx
@@ -3,5 +3,15 @@
 // 1330 => 1:30 PM
 
 export default (time: string | number): string | false => {
-    return '';
+    if(typeof(time) === 'string') {
+        time = Number(time.includes(':') ? time.replace(':', '') : time)
+    }
+    let minutes = (time%100), hours = (time - minutes)/100
+    if(minutes >= 60) {
+        hours++
+        minutes -= 60
+    }
+    let result = `${(hours%12 == 0) ? '12' : hours%12}:${String(minutes).padStart(2, '0')}`
+    result += Math.floor(hours/12) % 2 == 0 ? 'AM' : 'PM'
+    return result
 }
