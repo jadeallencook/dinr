11e76b34dd1217a80849ad5778cb431909bd0f52
Jade Allen Cook <jadeallencook@gmail.com>
2020-02-10 21:10:39 -0800

    🥘 Built out the UI for the host page

--- src/containers/Browse/index.tsx
+++ src/containers/Browse/index.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import './style.scss';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import zipcodeToLocation from '../../helpers/zipcode-to-location';
 import locationToUrl from '../../helpers/location-to-url';
 
 const BrowseComponent: React.FC = () => {
   const zipcode = useSelector(state => state['zipcode']);
+  const profile = useSelector(state => state['profile']);
   const location = zipcode ? zipcodeToLocation(zipcode) : null;
   const url = location ? locationToUrl(location.replace(':', ',')) : null;
-  
+
   return (
     <div className="BrowseComponent container">
-      <div className="banner">
+      <Link
+        to={
+          profile &&
+          profile.personal &&
+          profile.personal.zipcode &&
+          profile.personal.street &&
+          profile.personal.name
+            ? '/create'
+            : '/account'
+        }
+        className="banner"
+      >
         <p>HOST YOUR OWN</p>
         <h1>DINR</h1>
-      </div>
+      </Link>
       <h2>Featured Dinners</h2>
       <p>There are currently no listings...</p>
     </div>
   );
-}
+};
 
 export default BrowseComponent;
--- src/containers/Create/index.tsx
+++ src/containers/Create/index.tsx
@@ -3,10 +3,78 @@ import './style.scss';
 
 const CreateComponent: React.FC = () => {
   return (
-    <div className="CreateComponent">
-      <h1>Create is working...</h1>
+    <div className="CreateComponent container">
+      <h2>Host Dinner</h2>
+      <form>
+        <label>Title</label>
+        <input
+          className="brand margin-bottom"
+          type="text"
+          placeholder="Taco Night"
+        />
+        <label>Price (USD)</label>
+        <input className="brand margin-bottom" type="number" placeholder="8" />
+        <label>Plates</label>
+        <input className="brand margin-bottom" type="number" placeholder="5" />
+        <label>Date</label>
+        <select
+          className="brand margin-right"
+          defaultValue={new Date().getMonth() + 1}
+        >
+          {new Array(12).fill(null).map((value, index) => {
+            return (
+              <option value={index + 1} key={`month-${index}`}>
+                {index + 1}
+              </option>
+            );
+          })}
+        </select>
+        <select
+          className="brand margin-right"
+          defaultValue={new Date().getDate()}
+        >
+          {new Array(31).fill(null).map((value, index) => {
+            return (
+              <option value={index + 1} key={`month-${index}`}>
+                {index + 1}
+              </option>
+            );
+          })}
+        </select>
+        <select className="brand margin-bottom">
+          <option>2020</option>
+        </select>
+        <label>Time</label>
+        <select className="brand margin-right" defaultValue={'6'}>
+          {new Array(12).fill(null).map((value, index) => {
+            return (
+              <option value={index + 1} key={`month-${index}`}>
+                {index + 1}
+              </option>
+            );
+          })}
+        </select>
+        <select className="brand margin-right" defaultValue="00">
+          <option>00</option>
+          <option>15</option>
+          <option>30</option>
+          <option>45</option>
+        </select>
+        <select className="brand margin-bottom" defaultValue="PM">
+          <option>AM</option>
+          <option>PM</option>
+        </select>
+        <label>Description</label>
+        <textarea
+          placeholder="Say something about your dinner..."
+          className="brand"
+        ></textarea>
+        <br />
+        <br />
+        <input className="brand primary-bg" type="submit" value="Post Dinner" />
+      </form>
     </div>
   );
-}
+};
 
 export default CreateComponent;
