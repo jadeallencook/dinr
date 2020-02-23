a4fc95c57d09e497fcfb4cd6316cc6d8f4e314ac
Jade Allen Cook <jadeallencook@gmail.com>
2020-02-22 19:52:05 -0800

    👩‍🍳 Added Firebase API for adding dinners

--- src/containers/Create/index.tsx
+++ src/containers/Create/index.tsx
@@ -1,25 +1,118 @@
 import React from 'react';
 import './style.scss';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import { useSelector, useDispatch } from 'react-redux';
+import zipcodeToLocation from '../../helpers/zipcode-to-location';
+import locationToUrl from '../../helpers/location-to-url';
+import dinnerTemp from '../../templates/dinner.json';
 
 const CreateComponent: React.FC = () => {
+  const profile = useSelector(state => state['profile']);
+  const user = useSelector(state => state['user']);
+  const dispatch = useDispatch();
   return (
     <div className="CreateComponent container">
       <h2>Host Dinner</h2>
-      <form>
+      <form
+        onSubmit={(event: any) => {
+          event.preventDefault();
+          const form = event.target;
+          const title = form.querySelector('input#title').value;
+          const description = form.querySelector('textarea#description').value;
+          const month = form.querySelector('select#month').value;
+          const day = form.querySelector('select#day').value;
+          const year = form.querySelector('select#year').value;
+          const hour = form.querySelector('select#hour').value;
+          const minutes = form.querySelector('select#minutes').value;
+          const ampm = form.querySelector('select#ampm').value;
+          const price = Number(form.querySelector('input#price').value);
+          const plates = Number(form.querySelector('input#plates').value);
+          const date = new Date(
+            `${month}/${day}/${year} ${hour}:${minutes} ${ampm}`
+          );
+          if (new Date() > date) {
+            dispatch({
+              type: 'ADD_NOTIFICATION',
+              payload: {
+                type: 'secondary',
+                text: 'Invalid date'
+              }
+            });
+          } else {
+            const datestamp = date.toUTCString();
+            const location =
+              zipcodeToLocation(profile?.personal?.zipcode) || '';
+            const ref = `${locationToUrl(
+              location
+            )}_${date.toLocaleDateString().replace(/\//g, '-')}`;
+            firebase
+              .database()
+              .ref(`dinners/${ref}`)
+              .push({
+                ...dinnerTemp,
+                ...{
+                  ...{ title, price, plates, datestamp, description },
+                  profile: user.uid
+                }
+              })
+              .then(() => {
+                dispatch({
+                  type: 'ADD_NOTIFICATION',
+                  payload: {
+                    type: 'primary',
+                    text: 'Successfully added your dinner!'
+                  }
+                });
+                window.location.hash = '#/';
+              })
+              .catch(error => {
+                dispatch({
+                  type: 'ADD_NOTIFICATION',
+                  payload: {
+                    type: 'secondary',
+                    text: error.message
+                  }
+                });
+              });
+          }
+        }}
+      >
+        <label>Address</label>
+        <span>
+          {profile?.personal?.street} {profile?.personal?.zipcode}
+        </span>
+        <br />
+        <br />
         <label>Title</label>
         <input
           className="brand margin-bottom"
           type="text"
           placeholder="Taco Night"
+          id="title"
+          required
         />
         <label>Price (USD)</label>
-        <input className="brand margin-bottom" type="number" placeholder="8" />
+        <input
+          className="brand margin-bottom"
+          type="number"
+          placeholder="8"
+          id="price"
+          required
+        />
         <label>Plates</label>
-        <input className="brand margin-bottom" type="number" placeholder="5" />
+        <input
+          className="brand margin-bottom"
+          type="number"
+          placeholder="5"
+          id="plates"
+          required
+        />
         <label>Date</label>
         <select
           className="brand margin-right"
           defaultValue={new Date().getMonth() + 1}
+          id="month"
         >
           {new Array(12).fill(null).map((value, index) => {
             return (
@@ -32,6 +125,7 @@ const CreateComponent: React.FC = () => {
         <select
           className="brand margin-right"
           defaultValue={new Date().getDate()}
+          id="day"
         >
           {new Array(31).fill(null).map((value, index) => {
             return (
@@ -41,11 +135,11 @@ const CreateComponent: React.FC = () => {
             );
           })}
         </select>
-        <select className="brand margin-bottom">
+        <select className="brand margin-bottom" id="year">
           <option>2020</option>
         </select>
         <label>Time</label>
-        <select className="brand margin-right" defaultValue={'6'}>
+        <select className="brand margin-right" defaultValue={'6'} id="hour">
           {new Array(12).fill(null).map((value, index) => {
             return (
               <option value={index + 1} key={`month-${index}`}>
@@ -54,13 +148,13 @@ const CreateComponent: React.FC = () => {
             );
           })}
         </select>
-        <select className="brand margin-right" defaultValue="00">
+        <select className="brand margin-right" defaultValue="00" id="minutes">
           <option>00</option>
           <option>15</option>
           <option>30</option>
           <option>45</option>
         </select>
-        <select className="brand margin-bottom" defaultValue="PM">
+        <select className="brand margin-bottom" defaultValue="PM" id="ampm">
           <option>AM</option>
           <option>PM</option>
         </select>
@@ -68,6 +162,8 @@ const CreateComponent: React.FC = () => {
         <textarea
           placeholder="Say something about your dinner..."
           className="brand"
+          id="description"
+          required
         ></textarea>
         <br />
         <br />
--- src/helpers/location-to-url.tsx
+++ src/helpers/location-to-url.tsx
@@ -1,7 +1,8 @@
 // San Jose, CA => sanjose_ca
 
 export default (location: string): string => {
-  let [city, state] = location.split(',');
+  let [city, state] =
+    location.indexOf(',') !== -1 ? location.split(',') : location.split(':');
   city = city.replace(/\s/g, '').toLowerCase();
   state = state.replace(' ', '').toLowerCase();
   return `${city}_${state}`;
