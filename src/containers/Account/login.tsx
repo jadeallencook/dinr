c56b186321c4ae8d09e875d10ddbb24508d19862
Jade Allen Cook <jadeallencook@gmail.com>
2020-03-08 10:58:33 -0700

    Added account creation functionality

--- src/containers/Account/login.tsx
+++ src/containers/Account/login.tsx
@@ -5,26 +5,53 @@ import { useDispatch } from 'react-redux';
 
 const LoginComponent: React.FC = () => {
   const dispatch = useDispatch();
+
+  const loginHandler = (email: string, password: string) =>
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(error =>
+        dispatch({
+          type: 'ADD_NOTIFICATION',
+          payload: {
+            type: 'secondary',
+            text: error.message
+          }
+        })
+      );
+
+  const createAccountHandler = (email: string, password: string) =>
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => loginHandler(email, password))
+      .catch(error =>
+        dispatch({
+          type: 'ADD_NOTIFICATION',
+          payload: {
+            type: 'secondary',
+            text: error.message
+          }
+        })
+      );
+
   return (
     <div className="LoginComponent">
       <h2>Let's get cookin!</h2>
       <form
         onSubmit={event => {
+          event.preventDefault();
           const form: any = event.target;
           const email: string = form.querySelector('input#email').value;
           const password: string = form.querySelector('input#password').value;
-          firebase
-            .auth()
-            .signInWithEmailAndPassword(email, password)
-            .catch(error =>
-              dispatch({
-                type: 'ADD_NOTIFICATION',
-                payload: {
-                  type: 'secondary',
-                  text: error.message
-                }
-              })
-            );
+          const action: string | null = document.activeElement
+            ? document.activeElement.getAttribute('data-action')
+            : '';
+          if (action === 'create') {
+            createAccountHandler(email, password);
+          } else {
+            loginHandler(email, password);
+          }
         }}
       >
         <label>Email</label>
@@ -42,8 +69,15 @@ const LoginComponent: React.FC = () => {
           id="password"
         />
         <span>Forgot password?</span>
-        <input type="submit" className="brand brand-bg" value="Sign In" />
-        <button className="brand primary-bg">Create Account</button>
+        <input
+          data-action="login"
+          type="submit"
+          className="brand brand-bg"
+          value="Sign In"
+        />
+        <button data-action="create" className="brand primary-bg">
+          Create Account
+        </button>
       </form>
     </div>
   );
