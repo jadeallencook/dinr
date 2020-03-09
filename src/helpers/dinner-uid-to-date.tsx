c42e7d20b12472ca9abf76ffc5dd5f9ff63b5fc7
Jade Allen Cook <jadeallencook@gmail.com>
2020-03-08 21:49:18 -0700

    Updated reservations filter

---
+++ src/helpers/dinner-uid-to-date.tsx
@@ -0,0 +1,15 @@
+/*
+
+filters out reservations before the current date
+example: if today was 3/8/2020
+
+"dinners/sanjose_ca_3_2020/3_7_2020_6_00_PM_nfYF8vwm8MUCXR6ixTFxMAYlqjw2"
+=> new Date("3/7/2020 6:00 PM")
+
+*/
+
+export default (ref: string): Date => {
+  const uid = ref.split('/')[2];
+  const x = uid.split('_');
+  return new Date(`${x[0]}/${x[1]}/${x[2]} ${x[3]}:${x[4]} ${x[5]}`);
+};
--- src/helpers/filter-reservations.tsx
+++ src/helpers/filter-reservations.tsx
@@ -4,21 +4,21 @@ filters out reservations before the current date
 example: if today was 3/8/2020
 
 {
-    -M1kmBUpkZ8nQSZG4T4F: "dinners/sanjose_ca_3-7-2020/-M1kmBUpkZ8nQSZG4T4F"
-    -M1nM5DhfBm-6ZwurDQK: "dinners/sanjose_ca_3-9-2020/-M1nM5DhfBm-6ZwurDQK"
+    3_7_2020_6_00_PM_nfYF8vwm8MUCXR6ixTFxMAYlqjw2: "dinners/sanjose_ca_3_2020/3_7_2020_6_00_PM_nfYF8vwm8MUCXR6ixTFxMAYlqjw2"
+    3_8_2020_6_00_PM_nfYF8vwm8MUCXR6ixTFxMAYlqjw2: "dinners/sanjose_ca_3_2020/3_8_2020_6_00_PM_nfYF8vwm8MUCXR6ixTFxMAYlqjw2"
 } => [
-    "dinners/sanjose_ca_3-7-2020/-M1kmBUpkZ8nQSZG4T4F", 
-    "dinners/sanjose_ca_3-9-2020/-M1nM5DhfBm-6ZwurDQK"
+    "dinners/sanjose_ca_3_2020/3_8_2020_6_00_PM_nfYF8vwm8MUCXR6ixTFxMAYlqjw2"
 ]
 
 */
 
+import dinnerUidToDate from './dinner-uid-to-date';
+
 export default (reservations: object): string[] => {
   return Object.keys(reservations).filter((key: string) => {
-    const reservation = reservations[key].replace('dinners/', '');
-    const date = new Date(reservation.split('/')[0].split('_')[2]);
-    let today = new Date();
-    today.setHours(0, 0, 0, 0);
+    const ref = reservations[key];
+    const date = dinnerUidToDate(ref);
+    const today = new Date();
     return date >= today;
   }).map(key => reservations[key]);
 };
