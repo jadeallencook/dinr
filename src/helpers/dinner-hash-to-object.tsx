0ae7ba862ad2542c5849431cb3c3f2370b5f1f1c
Jade Allen Cook <jadeallencook@gmail.com>
2020-03-09 01:44:03 -0700

    Helper to convert dinner hashes to objects

---
+++ src/helpers/dinner-hash-to-object.tsx
@@ -0,0 +1,33 @@
+/*
+    "#/dinners/sanjose_ca_3_2020/3_9_2020_6_00_PM_K0bwbytfpDP8ZyzuDfhXTXxEPJR2"
+    => {
+        city: "sanjose",
+        state: "ca",
+        month: 3,
+        day: 9,
+        year: 2020,
+        hour: "6",
+        minute: "00",
+        ampm: "PM",
+        datestamp: "3/9/2020 6:00 PM",
+        uid: "K0bwbytfpDP8ZyzuDfhXTXxEPJR2"
+    }
+*/
+
+export default (hash: string): object => {
+  const a1 = hash.split('/');
+  const a2 = a1[2].split('_');
+  const a3 = a1[3].split('_');
+  return {
+    city: a2[0],
+    state: a2[1],
+    month: a3[0],
+    day: a3[1],
+    year: a3[2],
+    hour: a3[3],
+    minute: a3[4],
+    ampm: a3[5],
+    datestamp: `${a3[0]}/${a3[1]}/${a3[2]} ${a3[3]}:${a3[4]} ${a3[5]}`,
+    uid: a3[6]
+  };
+};
